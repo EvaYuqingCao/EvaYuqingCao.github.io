926be973b2a22cff8a328acdd035f14c9ef5a6cc
Yuzhen Zhang <68655092+AdamYuzhenZhang@users.noreply.github.com>
2022-01-21 19:49:49 -0500

    Added basic information

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,182 +4,298 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
+(function ($) {
 
-	var	$window = $(window),
+	var $window = $(window),
 		$body = $('body');
 
 	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '361px',   '480px'  ],
-			xxsmall:  [ null,      '360px'  ]
-		});
+	breakpoints({
+		xlarge: ['1281px', '1680px'],
+		large: ['981px', '1280px'],
+		medium: ['737px', '980px'],
+		small: ['481px', '736px'],
+		xsmall: ['361px', '480px'],
+		xxsmall: [null, '360px']
+	});
 
 	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+	$window.on('load', function () {
+		window.setTimeout(function () {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
 
 	// Touch?
-		if (browser.mobile)
-			$body.addClass('is-touch');
+	if (browser.mobile)
+		$body.addClass('is-touch');
 
 	// Forms.
-		var $form = $('form');
+	var $form = $('form');
 
-		// Auto-resizing textareas.
-			$form.find('textarea').each(function() {
+	// Auto-resizing textareas.
+	$form.find('textarea').each(function () {
 
-				var $this = $(this),
-					$wrapper = $('<div class="textarea-wrapper"></div>'),
-					$submits = $this.find('input[type="submit"]');
+		var $this = $(this),
+			$wrapper = $('<div class="textarea-wrapper"></div>'),
+			$submits = $this.find('input[type="submit"]');
+
+		$this
+			.wrap($wrapper)
+			.attr('rows', 1)
+			.css('overflow', 'hidden')
+			.css('resize', 'none')
+			.on('keydown', function (event) {
+
+				if (event.keyCode == 13
+					&& event.ctrlKey) {
+
+					event.preventDefault();
+					event.stopPropagation();
+
+					$(this).blur();
+
+				}
+
+			})
+			.on('blur focus', function () {
+				$this.val($.trim($this.val()));
+			})
+			.on('input blur focus --init', function () {
+
+				$wrapper
+					.css('height', $this.height());
 
 				$this
-					.wrap($wrapper)
-					.attr('rows', 1)
-					.css('overflow', 'hidden')
-					.css('resize', 'none')
-					.on('keydown', function(event) {
+					.css('height', 'auto')
+					.css('height', $this.prop('scrollHeight') + 'px');
 
-						if (event.keyCode == 13
-						&&	event.ctrlKey) {
+			})
+			.on('keyup', function (event) {
 
-							event.preventDefault();
-							event.stopPropagation();
+				if (event.keyCode == 9)
+					$this
+						.select();
 
-							$(this).blur();
+			})
+			.triggerHandler('--init');
 
-						}
+		// Fix.
+		if (browser.name == 'ie'
+			|| browser.mobile)
+			$this
+				.css('max-height', '10em')
+				.css('overflow-y', 'auto');
 
-					})
-					.on('blur focus', function() {
-						$this.val($.trim($this.val()));
-					})
-					.on('input blur focus --init', function() {
-
-						$wrapper
-							.css('height', $this.height());
-
-						$this
-							.css('height', 'auto')
-							.css('height', $this.prop('scrollHeight') + 'px');
-
-					})
-					.on('keyup', function(event) {
-
-						if (event.keyCode == 9)
-							$this
-								.select();
-
-					})
-					.triggerHandler('--init');
-
-				// Fix.
-					if (browser.name == 'ie'
-					||	browser.mobile)
-						$this
-							.css('max-height', '10em')
-							.css('overflow-y', 'auto');
-
-			});
+	});
 
 	// Menu.
-		var $menu = $('#menu');
+	var $menu = $('#menu');
 
-		$menu.wrapInner('<div class="inner"></div>');
+	$menu.wrapInner('<div class="inner"></div>');
 
-		$menu._locked = false;
+	$menu._locked = false;
 
-		$menu._lock = function() {
+	$menu._lock = function () {
 
-			if ($menu._locked)
-				return false;
+		if ($menu._locked)
+			return false;
 
-			$menu._locked = true;
+		$menu._locked = true;
 
-			window.setTimeout(function() {
-				$menu._locked = false;
+		window.setTimeout(function () {
+			$menu._locked = false;
+		}, 350);
+
+		return true;
+
+	};
+
+	$menu._show = function () {
+
+		if ($menu._lock())
+			$body.addClass('is-menu-visible');
+
+	};
+
+	$menu._hide = function () {
+
+		if ($menu._lock())
+			$body.removeClass('is-menu-visible');
+
+	};
+
+	$menu._toggle = function () {
+
+		if ($menu._lock())
+			$body.toggleClass('is-menu-visible');
+
+	};
+
+	$menu
+		.appendTo($body)
+		.on('click', function (event) {
+			event.stopPropagation();
+		})
+		.on('click', 'a', function (event) {
+
+			var href = $(this).attr('href');
+
+			event.preventDefault();
+			event.stopPropagation();
+
+			// Hide.
+			$menu._hide();
+
+			// Redirect.
+			if (href == '#menu')
+				return;
+
+			window.setTimeout(function () {
+				window.location.href = href;
 			}, 350);
 
-			return true;
+		})
+		.append('<a class="close" href="#menu">Close</a>');
 
-		};
+	$body
+		.on('click', 'a[href="#menu"]', function (event) {
 
-		$menu._show = function() {
+			event.stopPropagation();
+			event.preventDefault();
 
-			if ($menu._lock())
-				$body.addClass('is-menu-visible');
+			// Toggle.
+			$menu._toggle();
 
-		};
+		})
+		.on('click', function (event) {
 
-		$menu._hide = function() {
+			// Hide.
+			$menu._hide();
 
-			if ($menu._lock())
-				$body.removeClass('is-menu-visible');
+		})
+		.on('keydown', function (event) {
 
-		};
+			// Hide on escape.
+			if (event.keyCode == 27)
+				$menu._hide();
 
-		$menu._toggle = function() {
+		});
 
-			if ($menu._lock())
-				$body.toggleClass('is-menu-visible');
 
-		};
+	// Project.
+	var $project = $('#project');
 
-		$menu
-			.appendTo($body)
-			.on('click', function(event) {
-				event.stopPropagation();
-			})
-			.on('click', 'a', function(event) {
+	$project.wrapInner('<div class="inner"></div>');
 
-				var href = $(this).attr('href');
+	$project._locked = false;
 
-				event.preventDefault();
-				event.stopPropagation();
+	$project._lock = function () {
 
-				// Hide.
-					$menu._hide();
+		if ($project._locked)
+			return false;
 
-				// Redirect.
-					if (href == '#menu')
-						return;
+		$project._locked = true;
 
-					window.setTimeout(function() {
-						window.location.href = href;
-					}, 350);
+		window.setTimeout(function () {
+			$project._locked = false;
+		}, 350);
 
-			})
-			.append('<a class="close" href="#menu">Close</a>');
+		return true;
 
-		$body
-			.on('click', 'a[href="#menu"]', function(event) {
+	};
 
-				event.stopPropagation();
-				event.preventDefault();
+	$project._show = function () {
 
-				// Toggle.
-					$menu._toggle();
+		if ($project._lock())
+			$body.addClass('is-project-visible');
 
-			})
-			.on('click', function(event) {
+	};
 
-				// Hide.
-					$menu._hide();
+	$project._hide = function () {
 
-			})
-			.on('keydown', function(event) {
+		if ($project._lock())
+			$body.removeClass('is-project-visible');
 
-				// Hide on escape.
-					if (event.keyCode == 27)
-						$menu._hide();
+	};
 
-			});
+	$project._toggle = function () {
+
+		if ($project._lock())
+			$body.toggleClass('is-project-visible');
+
+	};
+
+	$project
+		.appendTo($body)
+		.on('click', function (event) {
+			event.stopPropagation();
+		})
+		.on('click', 'a', function (event) {
+
+			var href = $(this).attr('href');
+
+			event.preventDefault();
+			event.stopPropagation();
+
+			// Hide.
+			$project._hide();
+
+			// Redirect.
+			if (href == '#project')
+				return;
+
+			window.setTimeout(function () {
+				window.location.href = href;
+			}, 350);
+
+		})
+		.append('<a class="close" href="#project">Close</a>');
+
+	$body
+		.on('click', 'a[href="#project"]', function (event) {
+
+			event.stopPropagation();
+			event.preventDefault();
+
+			// Toggle.
+			$project._toggle();
+
+		})
+		.on('click', function (event) {
+
+			// Hide.
+			$project._hide();
+
+		})
+		.on('keydown', function (event) {
+
+			// Hide on escape.
+			if (event.keyCode == 27)
+				$project._hide();
+
+		});
 
 })(jQuery);
+
+function hideProjects(p) {
+	var x = document.getElementsByClassName("p");
+	for (var i = 0; i < x.length; i++) {
+		if (x[i].id == p) {
+			x[i].style.display = "block";
+		} else {
+			x[i].style.display = "none";
+		}
+	}
+}
+
+function filterProjects(p) {
+	var x = document.getElementsByClassName("pIcon");
+	for (var i = 0; i < x.length; i++) {
+		if (x[i].classList.contains(p)) {
+			x[i].style.display = "block";
+		} else {
+			x[i].style.display = "none";
+		}
+	}
+}
